7b03517b03a1a9259fc65804e5f5e9a68842f191
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-05-30 16:55:11 +0200

    cache the weather view for a minute

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.cache import cache_page
+
 
 import pywapi
 
+@cache_page(60) # seconds
 def weather(request):
     locations = ["94041", "90008", "97202", "94110", "10024",
                  "02111", "60605", "23454", "32804", "75204",
